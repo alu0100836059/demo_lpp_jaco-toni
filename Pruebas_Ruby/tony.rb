f8164c15c959ef45d7fa180aca1b685cabcec07d
alu0100789331 <alu0100789331@edu.ull.es>
2017-01-16 15:00:44 +0000

    añadiendo mierda

---
+++ Pruebas_Ruby/tony.rb
@@ -0,0 +1,25 @@
+class Numero
+   attr_accessor :numerador_, :denominador_ 
+    def  initialize(*args)
+        if(args.count()==0)
+            @numerador_ = 0
+            @denominador_ = 0
+        elsif(args.count()==2)
+            @numerador_ = args[0]
+            @denominador_ = args[1]
+        else
+            "fallo de construcctor"
+        end
+    end  
+    
+    def to_s
+        "el numero es:#{@numerador_}/#{@denominador_}"
+    end
+end
+
+#numero = Numero.new(1,2)
+#puts numero
+
+class Menu
+    
+end
