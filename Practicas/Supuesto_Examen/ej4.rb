fe9a9bab2d88f511c9b960292701b83886cea635
alu0100836059 <alu0100836059@ull.edu.es>
2017-01-17 17:30:06 +0000

    +ej4.rb completado

--- Practicas/Supuesto_Examen/ej4.rb
+++ Practicas/Supuesto_Examen/ej4.rb
@@ -11,6 +11,82 @@
   El código que sigue a continuación supone esta última aserción.
 =end
 
-def max_VCM
-  
+##### clase del ej2.rb ####
+class Menu_dietetico
+
+  # Incluimos el módulo comparable para poder hacer comparaciones y el módulo
+  # enumerable para poder hacer uso de las funciones min max
+  include Comparable
+  include Enumerable
+  ##################
+
+  attr_accessor :id, :platos, :VCM
+
+  def initialize (identificador, conjunto_platos, calorico_max)
+    @id = identificador
+    @platos = conjunto_platos
+    @VCM = calorico_max
+  end
+
+  def string_platos
+    salida = ""
+    @platos.each do |plato|
+      salida += "\t#{plato}\n"
+    end
+    salida
+  end
+
+  # Nuevo ej4.rb
+  # Para el módulo Comparable
+  def <=> other
+    self.VCM <=> other.VCM
+  end
+
+  # Para el módulo Enumerable
+  # def each
+  #
+  # end
+  ##############
+
+
+
+  def to_s
+    salida = ""
+    salida += "Identificador del menú: #{@id}\n"+
+              "Conjunto de platos:\n"+ self.string_platos +
+              "V.C.M = #{@VCM} kcal"
+    salida
+  end
 end
+#############################
+
+# Creando varios menús para hacer una recomendación
+ligero_mañana = Menu_dietetico.new(1, ["Zumo de naranja", "Pieza de fruta",
+                                "Leche o café con poco azúcar"], 177)
+
+ligero_mediodia = Menu_dietetico.new(2, ["Ensalada de col de bruselas", "Filete de pavo a la plancha",
+                "Macedonia de frutas"], 317)
+
+ligero_mediatarde = Menu_dietetico.new(3, ["Pieza de fruta", "Iogurt con frutos secos",
+                "Té o agua con limón"], 117)
+
+ligero_noche = Menu_dietetico.new(4, ["Pollo a la plancha", "Filete de pavo a la plancha"], 137)
+
+
+recomendacion = [ligero_mañana, ligero_mediodia, ligero_mediatarde, ligero_noche]
+
+
+# Mostramos para ver la correcta inserción de los datos:
+recomendacion.each do |r|
+  puts r
+end
+
+
+#### ej4.rb Función que se pide en el ejercicio
+def max_VCM conjunto
+  maximo = conjunto.max
+  maximo
+end
+
+# Mostramos por pantalla:
+puts "\nEl menú con un máximo de calorías es :\n#{max_VCM recomendacion}"
