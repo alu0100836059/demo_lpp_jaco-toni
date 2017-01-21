61a8a2184f6acc589621bf85aebd4734fec839f8
alu0100836059 <alu0100836059@ull.edu.es>
2017-01-21 20:35:23 +0000

    Comenzando con la programación concurrente

---
+++ Pruebas_Ruby/HILOS.rb
@@ -0,0 +1,8 @@
+# Comenzando con la programación concurrente a través de hilos en Ruby
+
+personas = ["Juan", "Pedro", "Maria", "Carla", "Lucia"]
+
+hilos = Array.new
+
+personas.each do |x|
+# SOLO EMPEZADO, SEGUIR CON EL EJEMPLO SENCILLO DE SALUDAR MEDIANTE HILOS
